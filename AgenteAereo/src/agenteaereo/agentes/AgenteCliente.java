740f82c814b08e6aa7cc99133779f834abce378f
Ximenes130 <ximenes130@gmail.com>
2016-03-23 11:03:38 -0300

    atualizando

--- AgenteAereo/src/agenteaereo/agentes/AgenteCliente.java
+++ AgenteAereo/src/agenteaereo/agentes/AgenteCliente.java
@@ -8,6 +8,10 @@ package agenteaereo.agentes;
 import jade.core.AID;
 import jade.core.Agent;
 import jade.core.behaviours.WakerBehaviour;
+import jade.domain.DFService;
+import jade.domain.FIPAAgentManagement.DFAgentDescription;
+import jade.domain.FIPAAgentManagement.ServiceDescription;
+import jade.domain.FIPAException;
 import jade.lang.acl.ACLMessage;
 
 /**
@@ -15,6 +19,9 @@ import jade.lang.acl.ACLMessage;
  * @author Luiz Eduardo
  */
 public class AgenteCliente extends Agent{
+    
+    AID[] listaCompanhias;
+    
     protected void setup() {
         System.out.println("Novo agente rodando - " + getAID().getName() + ";");
         
@@ -23,15 +30,31 @@ public class AgenteCliente extends Agent{
             protected void handleElapsedTimeout() {
                 System.out.println("Requisitando voos;");
                 
+                
+                 // Update the list of seller agents
+                DFAgentDescription filtro = new DFAgentDescription();
+                ServiceDescription serviceDescription = new ServiceDescription();
+                serviceDescription.setType("companhia-aerea");
+                filtro.addServices(serviceDescription);
+                try {
+                    DFAgentDescription[] resultado = DFService.search(myAgent, filtro);
+                    listaCompanhias = new AID[resultado.length];
+                    
+                    for (int i = 0; i < resultado.length; ++i) {
+                        listaCompanhias[i] = resultado[i].getName();
+                    }
+                }
+                catch (FIPAException fe) {
+                    fe.printStackTrace();
+                }
+                
                 // Criando objeto de mensagem
                 ACLMessage mensagem = new ACLMessage(ACLMessage.CFP);
-                /*
-                for (int i = 0; i < sellerAgents.lenght; ++i) {
-                    mensagem.addReceiver(sellerAgents[i]);
-                }
-                */
                 
-                mensagem.addReceiver(new AID("jonas", true));
+                for (int i = 0; i < listaCompanhias.length; ++i) {
+                    mensagem.addReceiver(listaCompanhias[i]);
+                    // System.out.println("sms("+i+") - "+ listaCompanhias[i].getName());
+                }
                 
                 mensagem.setContent("requisito-voos");
                 myAgent.send(mensagem);
